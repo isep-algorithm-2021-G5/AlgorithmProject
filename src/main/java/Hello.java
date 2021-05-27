40ef93f5b69ebbf8a44d71cd0d7dc65b9b40002b
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-05-27 18:06:51 +0800

    add junit 5

---
+++ src/main/java/Hello.java
@@ -0,0 +1,8 @@
+/**
+ * @author : Xuan MIAO
+ * @version : 1.0.0
+ * @date : 2021/5/27
+ */
+public class Hello
+{
+}
---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+/**
+ * @author : Xuan MIAO
+ * @version : 1.0.0
+ * @date : 2021/5/27
+ */
+class HelloTest
+{
+
+    @BeforeEach
+    void setUp()
+    {
+    }
+
+    @AfterEach
+    void tearDown()
+    {
+    }
+}
