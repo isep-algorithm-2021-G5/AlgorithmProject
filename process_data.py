c04f3a37e26af82c667aec03de4eedac608c4767
TioPlato <resets. 60922756+tio-plato@users.noreply.github.com>
2021-06-13 18:12:10 +0800

    update data processing

--- process_data.py
+++ process_data.py
@@ -1,10 +1,10 @@
-from collections import defaultdict
-import os
 import csv
-import json
+import os
+from collections import defaultdict
+from math import cos, sin, asin, sqrt, pi
 
 
-def getBusRoutes():
+def get_bus_routes():
     routes = set()
     with open(os.path.join("gtfs", "routes.txt"), encoding="utf-8") as f:
         f_csv = csv.DictReader(f)
@@ -14,7 +14,7 @@ def getBusRoutes():
     return routes
 
 
-def getTripsByRoutes(routes):
+def get_trips_by_routes(routes):
     trips = defaultdict(list)
     with open(os.path.join("gtfs", "trips.txt"), encoding="utf-8") as f:
         f_csv = csv.DictReader(f)
@@ -24,14 +24,14 @@ def getTripsByRoutes(routes):
     return trips
 
 
-def getTripsWithStops(trips):
-    tripsWithStops = defaultdict(list)
-    tripsWithStopsUnique = list()
+def get_trips_with_stops(trips):
+    trips_stops = defaultdict(list)
+    trips_stops_unique = list()
 
     with open(os.path.join("gtfs", "stop_times.txt"), encoding="utf-8") as f:
         f_csv = csv.DictReader(f)
         for row in f_csv:
-            tripsWithStops[row["trip_id"]].append(row["stop_id"])
+            trips_stops[row["trip_id"]].append(row["stop_id"])
 
     for routes in trips:
         flag = 0
@@ -39,55 +39,94 @@ def getTripsWithStops(trips):
         for tripId in trips[routes]:
             if flag == 0:
                 flag = 1
-                test = tripsWithStops[tripId]
-                tripsWithStopsUnique.append(test)
+                test = trips_stops[tripId]
+                trips_stops_unique.append(test)
             else:
-                v = tripsWithStops[tripId]
-                if v not in tripsWithStopsUnique:
+                v = trips_stops[tripId]
+                if v not in trips_stops_unique:
                     index += 1
-                    tripsWithStopsUnique.append(tripsWithStops[tripId])
+                    trips_stops_unique.append(trips_stops[tripId])
 
-    return tripsWithStopsUnique
+    return trips_stops_unique
 
 
-def getStops(tripsWithStops, adjList):
-    usedStops = set()
+def get_stops(trips):
+    used_stops = set()
     stops = defaultdict(dict)
-    for trip in tripsWithStops:
+    for trip in trips:
         for stop in trip:
-            usedStops.add(stop)
+            used_stops.add(stop)
 
     with open(os.path.join("gtfs", "stops.txt"), encoding="utf-8") as f:
         f_csv = csv.DictReader(f)
         for row in f_csv:
-            if row["stop_id"] in usedStops:
+            if row["stop_id"] in used_stops:
                 stops[row["stop_id"]] = {
                     "lat": float(row["stop_lat"]),
-                    "lon": float(row["stop_lon"]),
-                    "adjList": adjList[row["stop_id"]],
+                    "lon": float(row["stop_lon"])
                 }
     return stops
 
 
-def getAdjList(stopsUnique):
-    adjSet = defaultdict(set)
-    for s in stopsUnique:
+def get_adj_list(trips_stops_unique):
+    adj_set = defaultdict(set)
+    for s in trips_stops_unique:
         for i in range(0, len(s) - 1):
-            adjSet[s[i]].add(int(s[i + 1]))
-    adjList = defaultdict(list)
-    for key in adjSet:
-        adjList[key] = list(adjSet[key])
-    return adjList
+            adj_set[s[i]].add(int(s[i + 1]))
+    adj_list = defaultdict(list)
+    for key in adj_set:
+        adj_list[key] = list(adj_set[key])
+    return adj_list
 
 
-def writeAdjList(jsonIn):
-    with open(os.path.join("gtfs", "result.json"), encoding="utf-8", mode="w+") as f:
-        f.write(jsonIn)
+def get_graph(adj_list, stops):
+    graph = list()
+    for from_id in adj_list:
+        for to_id in adj_list[from_id]:
+            distance = get_distance(stops[from_id]["lat"],
+                                    stops[from_id]["lon"],
+                                    stops[str(to_id)]["lat"],
+                                    stops[str(to_id)]["lon"])
+            graph.append([int(from_id), int(to_id), distance])
+
+    return graph
+
+
+def write_csv(graph, stops):
+    with open(os.path.join("gtfs", "graph.csv"), encoding="utf-8",
+              mode="w+", newline='') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerows(graph)
+    stops_list = list()
+    for stop_id in stops:
+        stops_list.append([int(stop_id), stops[stop_id]["lat"],
+                           stops[stop_id]["lon"]])
+    with open(os.path.join("gtfs", "stops.csv"), encoding="utf-8",
+              mode="w+", newline='') as f:
+        f_csv = csv.writer(f)
+        f_csv.writerows(stops_list)
+
+
+def rad(d):
+    return d * pi / 180.0
+
+
+def get_distance(lat1, lon1, lat2, lon2):
+    rad_lat1 = rad(lat1)
+    rad_lat2 = rad(lat2)
+    a = rad_lat1 - rad_lat2
+    b = rad(lon1) - rad(lon2)
+    s = 2 * asin(sqrt(
+            pow(sin(a / 2), 2) + cos(rad_lat1) * cos(rad_lat2) * pow(
+                    sin(b / 2), 2)))
+    s = s * 6378.137
+    return s
 
 
 if __name__ == "__main__":
-    tripsWithStops = getTripsWithStops(getTripsByRoutes(getBusRoutes()))
-    stops = getStops(tripsWithStops,getAdjList(tripsWithStops))
-    jsonStops = json.dumps(stops)
-    writeAdjList(jsonStops)
-
+    trips_with_stops = get_trips_with_stops(
+            get_trips_by_routes(get_bus_routes()))
+    adj_list_res = get_adj_list(trips_with_stops)
+    stops_res = get_stops(trips_with_stops)
+    graph_res = get_graph(adj_list_res, stops_res)
+    write_csv(graph_res,stops_res)
