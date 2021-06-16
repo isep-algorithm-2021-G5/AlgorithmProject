518b3147ad95b9cb25e5d5b7664a18644bec60d7
xianqiliu <xianqi.liu@eleve.isep.fr>
2021-06-16 21:20:58 +0200

    [4]BFS IN MAIN

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import algorithm.BfsShortestPath;
 import algorithm.DijkstraShortestPath;
 import algorithm.YenKsp;
 import config.Path;
@@ -6,6 +7,8 @@ import graph.ShortestPath;
 import java.util.Collections;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.TreeSet;
+
 import utils.Mapping;
 import utils.Visualization;
 
@@ -31,10 +34,12 @@ public class Main
         System.out.println("[1] Bus stop graph of Phoenix");
         System.out.println("[2] Dijkstra's shortest path");
         System.out.println("[3] K-shortest paths(It will take one to two minutes)");
+        System.out.println("[4] BFS shortest path");
         Scanner in = new Scanner(System.in);
         Graph graph = new Graph(Path.PHOENIX_STOPS, Path.PHOENIX_GRAPH, 0);
         Graph weightedGraph = new Graph(Path.PHOENIX_STOPS, Path.PHOENIX_WEIGHTED_GRAPH, 1);
         Graph mappedGraph = Mapping.reMapGraph(weightedGraph);
+        Graph mappedGraph2 = Mapping.reMapGraph(graph);
         switch (in.nextInt())
         {
             case 1:
@@ -57,6 +62,19 @@ public class Main
                     System.out.println(sp.getShortestPathList().toString());
                 }
                 Visualization.show(mappedGraph, sps, "Yen", true);
+                break;
+            case 4:
+                BfsShortestPath bfs = new BfsShortestPath(mappedGraph2,START);
+                ShortestPath sp = new ShortestPath(START,END,bfs.getShortestPath(END));
+                System.out.println("BFS Shortest Path: "+START+" -> "+END);
+                System.out.println("The stops number passed is: " + sp.getShortestPathList().size());
+                for(int i = 0; i < sp.getShortestPathList().size(); i++){
+                    System.out.print(sp.getShortestPathList().get(i) + "->");
+                    if(i % 20 == 0 && i != 0) System.out.println();
+                }
+                Set<ShortestPath> spSet = new TreeSet<>();
+                spSet.add(sp);
+                Visualization.show(mappedGraph2, spSet, "BFS", true);
                 break;
             default:
                 System.out.println("Wrong input");
